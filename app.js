a02486e905eabf0e6ecacbe72e6416bf2455aec4
Franamaus <64950097+franamaus@users.noreply.github.com>
2020-07-15 01:11:08 -0700

    added src folder to connect code from another js file // html created

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+// above two lines can be written as below, called "assignment restructuring"
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to the the output.');
+})
---
+++ src/page-template.js
@@ -0,0 +1,22 @@
+//when a function has only a single statement, the curly braces, {}, 
+//are unnecessary and the return statement is implied.
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+}
+
+module.exports = generatePage;
