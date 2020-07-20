f471102705a6dba31c9bd5b38b460f09b6f84d7a
Franamaus <64950097+franamaus@users.noreply.github.com>
2020-07-20 00:25:50 -0700

    coded promise in order to remove err callnacks in functions fro readability

---
+++ utils/generate-site.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File copied!'
+            })
+        })
+    })
+}
+
+// shorthand property name, if we are using the same key name with the same key value
+// module.exports = {
+//     writeFile: writeFile,
+//     copyFile: copyFile
+// };
+module.exports = { writeFile, copyFile };
